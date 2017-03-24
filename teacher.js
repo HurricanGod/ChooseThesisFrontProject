642e24c8ac042f03fc3ed95ad9c9bd05dc55f4a4
krys <709593732@qq.com>
2017-03-24 10:37:42 +0800

    修改教师页面

--- teacher.js
+++ teacher.js
@@ -74,7 +74,6 @@ $(document).ready(function(){
 		$(".allofthetopic").show();
 		$(".allofthetopic1").hide();
 		$(".deletestudent").hide();
-		$(".deletetopic").hide();
 		$(".selectstudent").hide();
 		$(".protocol").hide();
 		$(".thesistitle").hide();
@@ -85,8 +84,6 @@ $(document).ready(function(){
 		$(".allofthetopic1").show();
 		$(".allofthetopic").hide();
 		$(".deletestudent").hide();
-		$(".deletetopic").hide();
-		
 		$(".selectstudent").hide();
 		$(".protocol").hide();
 		$(".thesistitle").hide();
@@ -95,8 +92,6 @@ $(document).ready(function(){
 	$('.navigation button').eq(10).click(function(){
 		$(".manage").show()
 		$(".deletestudent").show();
-		$(".deletetopic").hide();
-		
 		$(".allofthetopic").hide();
 		$(".allofthetopic1").hide();
 		$(".selectstudent").hide();
@@ -104,26 +99,16 @@ $(document).ready(function(){
 	$(".thesistitle").hide();
 		
 	});
-	$('.navigation button').eq(11).click(function(){
-		$(".manage").show()
-		$(".deletestudent").hide();
-		$(".deletetopic").show();
-		$(".allofthetopic").hide();
-		$(".allofthetopic1").hide();
-		$(".selectstudent").hide();
-		$(".protocol").hide();
-	$(".thesistitle").hide();
-		
-	});
+
 	
-	$('.navigation button').eq(12).click(function(){
+	$('.navigation button').eq(11).click(function(){
 		$(".selectstudent").hide();
 		$(".manage").hide();
 		$(".protocol").show();
 		$(".thesistitle").hide();
 		
 	});
-	$(".navigation button").eq(13).click(function(){
+	$(".navigation button").eq(12).click(function(){
 		$(".selectstudent").hide();
 		$(".manage").hide();
 		$(".protocol").hide();
